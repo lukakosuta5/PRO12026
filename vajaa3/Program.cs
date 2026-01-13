4f8eaf6349190f9fd2ab7ab6e2d7d64b0e14f85d
študent <student@c.scng.si>
2026-01-13 10:38:42 +0100

    aaaa

---
+++ vajaa3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace vajaa3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("na kmetiji je lepo");
+            Console.WriteLine("I-A-I-A-I-A-O");
+            Console.WriteLine("TAM PA KRAVE MUKAJO");
+            Console.WriteLine("I-A-I-A-I-A-O");
+        }
+    }
+}
