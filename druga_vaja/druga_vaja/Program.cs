9768de3e2a54e8c6e22d6f8b7903cf511d873d43
študent <student@c.scng.si>
2026-01-13 10:10:39 +0100

    aaaa

---
+++ druga_vaja/druga_vaja/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace druga_vaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rad imam cshark");
+        }
+    }
+}
